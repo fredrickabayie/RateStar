ea87e51c47a327bd0cdf3b1fbb46270a7da30011
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-12-11 16:49:10 +0000

    Initial commit

---
+++ RateStar/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace RateStar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string result, url, comment, rating = "";
+
+            if (Convert.ToBoolean(worse.IsChecked))
+            {
+                rating = "1";
+               // MessageBox.Show("1 checked");
+            }
+            else if (Convert.ToBoolean(bad.IsChecked))
+            {
+                rating = "2";
+            }
+            else if (Convert.ToBoolean(ok.IsChecked))
+            {
+                rating = "3";
+            }
+            else if (Convert.ToBoolean(good.IsChecked))
+            {
+                rating = "4";
+            }
+            else if (Convert.ToBoolean(excellent.IsChecked))
+            {
+                rating = "5";
+            }
+
+            comment = commentTxt.Text;
+
+            url = "http://cs.ashesi.edu.gh/~csashesi/class2016/fredrick-abayie/mobileweb/bisa_codename1/php/bisa.php?cmd=add_rating&rating="+rating+"&comment="+comment;
+
+            HttpClient client = new HttpClient();
+            result = await client.GetStringAsync(url);
+
+            JObject jObject = JObject.Parse(result);
+
+           
+
+            MessageDialog messageDialog = new MessageDialog("App rated");
+            await messageDialog.ShowAsync();
+
+            commentTxt.Text = "";
+        }
+
+        //Button to load another page to view the results
+        private void resultPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1), null);
+            //NavigationService.Navigate(new Uri("/Results.xaml", UriKind.Relative));
+        }
+    }
+
+}
---
+++ RateStar/results.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace RateStar
+{
+    
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+
+        private HttpClient httpClient;
+        //private string url = "";
+        private string result = "";
+
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+                getRatings("http://cs.ashesi.edu.gh/~csashesi/class2016/fredrick-abayie/mobileweb/bisa_codename1/php/bisa.php?cmd=rating_view");
+        }
+
+        public async void getRatings(string url)
+        {
+            string feedback = "", rate = "", date = "";
+
+            InitializeComponent();
+            httpClient = new HttpClient();
+            result = await httpClient.GetStringAsync(url);
+
+            JObject jObject = JObject.Parse(result);
+
+            JArray jArray = (JArray)jObject["ratings"];
+            
+            for (int rating = 0; rating < jArray.Count; rating++)
+            {
+                //comment.Text = jArray[rating]["comment"] + "";
+                feedback = jArray[rating]["comment"] + "";
+                rate = jArray[rating]["rating"] + "";
+                date = jArray[rating]["date"] + "";
+
+                resultList.Items.Add("Comment: "+feedback+"\n Rating: "+rate+"\n Date: "+date+"\n");
+                //resultList.Items.Add(rate);
+            }
+            
+
+        }
+
+        private void addRating_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
